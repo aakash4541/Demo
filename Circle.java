17db217442c039c7519985c52e58cf8e9ae23cbb
Pavan R <155996080+PavanR46@users.noreply.github.com>
2024-05-25 11:22:26 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+interface shapes
+{
+final static double pi=3.142;
+public void area(float r);
+}
+class Circle implements shapes
+{
+float rad;
+float area_circle;
+public void area(float r);
+{
+rad=r;
+area_circle=(float)pi*rad*rad;
+system.out.println("area is"+area_circle);
+}
+public static void main(String args[])
+throws IOException
+{
+circle c=new circle();
+float radius;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the radius");
+radius=sc.nextInt();
+c.area(radius);
+}
+}
