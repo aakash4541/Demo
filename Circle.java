b4676c421252325b1bcdeaa61db28eca458dc048
Jishaaaaa <Jishaaaaa>
2024-05-27 08:44:37 +0530

    Demo

--- Circle.java
+++ Circle.java
@@ -23,6 +23,7 @@ float radius;
 Scanner sc=new Scanner(System.in);
 System.out.println("enter the radius");
 radius=sc.nextInt();
+system.out.println("area is"+area_circle);
 c.area(radius);
 c.area(radius- diameter)
 circle radius=2
